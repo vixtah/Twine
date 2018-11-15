617d247e4db4ddbb02157d9e7b9654c6ce2fa778
Victor <Victor@victors-macbook-pro.local>
2018-11-15 11:17:50 -0800

    Initial Commit

---
+++ Twine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twine
+//
+//  Created by Victor on 11/15/18.
+//  Copyright © 2018 Vixtah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
